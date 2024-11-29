aebc1740529a92c75810a77c11b1884da3f11678
Sevda <sevda.amini@codexconsulting.com.au>
2024-11-29 20:18:48 +1000

    custom hooks created

--- custom-hook-local-storage/src/App.tsx
+++ custom-hook-local-storage/src/App.tsx
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+import { Box, Button, Paper, TextField } from "@mui/material";
+import { FormData } from "./interface/Interface";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 function App() {
+  const [formData, setFormData] = useLocalStorage<FormData>("formData", {
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+    // prev refers to the previous state of formData before the update.
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted data:", formData);
+    // Save to localStorage
+    localStorage.setItem("formData", JSON.stringify(formData));
+    setFormData({ name: "", email: "" });
+  };
+
+  const handleRemove = () => {
+    localStorage.removeItem("formData");
+    setFormData({ name: "", email: "" });
+  };
+
+  const handleClear = () => {
+    localStorage.clear();
+    setFormData({ name: "", email: "" });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Paper
+        elevation={4}
+        sx={{
+          padding: 4,
+          borderRadius: 2,
+          width: "400px",
+          maxWidth: "90%",
+        }}
+      >
+        <TextField
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <TextField
+          label="Email"
+          name="email"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <Button type="submit" variant="contained">
+          Submit
+        </Button>
+      </Paper>
+      <Button
+        onClick={handleRemove}
+        variant="outlined"
+        color="error"
+        sx={{ mt: 2 }}
+      >
+        Remove from LocalStorage
+      </Button>
+      <Button onClick={handleClear}>Clear from LocalStorage</Button>
+    </Box>
   );
 }
 
